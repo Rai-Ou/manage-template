af2f4c2299b6a01531b780d01ad3938ebc2379d3
王磊 <63234875@163.com>
2020-12-17 10:26:53 +0800

    login

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,16 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
-export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
+export function login(query) {
+    return request({
+        url: "/tenant/user/login",
+        method: "post",
+        data: query
+    });
 }
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
-
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+export function getInfo() {
+    return request({
+        url: "/tenant/user/getCurrentUserInfo",
+        method: "post"
+    });
 }
